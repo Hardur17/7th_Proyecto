6383192954785df3bd38e0cd090841f04e20b4d4
Hardur17 <harolduran2005@gmail.com>
2026-05-10 15:19:26 -0500

    actu v2

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,16 +1,17 @@
 const express = require("express");
 
 const app = express();
+
 const PORT = 3000;
 
 app.use(express.json());
 
-// Ruta básica
+// ruta base
 app.get("/", (req, res) => {
   res.send("Servidor en línea");
 });
 
-// Login 
+// ruta de login
 app.post("/login", (req, res) => {
   const correo = req.body.correo;
   const password = req.body.password;
@@ -26,7 +27,7 @@ app.post("/login", (req, res) => {
   }
 });
 
-// prende server
+//encender server
 app.listen(PORT, () => {
   console.log("Servidor corriendo en el puerto " + PORT);
 });
